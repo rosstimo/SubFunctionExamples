7ae318ecc5cc1a64602337289658816cec1a9207
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2021-01-27 08:12:58 -0700

    Add project files.

---
+++ SubFunctionExamples.vb
@@ -0,0 +1,12 @@
+﻿Module SubFunctionExamples
+
+    Sub Main()
+        Console.WriteLine("I'm in sub main!!")
+        Console.Read()
+    End Sub
+
+    Sub MyOtherSub()
+        Console.WriteLine("I'm in my other sub!!")
+    End Sub
+
+End Module
