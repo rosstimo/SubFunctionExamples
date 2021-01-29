933b45e5af079bf411c80281188b6d83937403f3
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2021-01-29 08:11:54 -0700

    add special

---
+++ SpecialFunc.vb
@@ -0,0 +1,25 @@
+﻿Module SpecialFunc
+    Sub main()
+
+        promptUser("hello user")
+
+        For i = 0 To 5
+            storeMessages("hello" & vbNewLine) 'chr(13) & chr(11)
+        Next
+
+        promptUser(storeMessages(""))
+        Console.Read()
+    End Sub
+
+    Sub promptUser(message As String)
+        Console.WriteLine(message)
+    End Sub
+
+    Function storeMessages(message As String) As String
+        Static storedMessages As String 'static extends lifetime of the variable
+        storedMessages &= message
+        Return storedMessages
+    End Function
+
+
+End Module
